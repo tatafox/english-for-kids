316f8b0fea987eb85a610e37bb1a3623f9e9574f
tatafox <pavlovetstatyana@gmail.com>
2021-05-27 12:57:55 +0300

    fix: error card

--- burger.js
+++ burger.js
@@ -15,4 +15,4 @@ function burgerClick(e) {
   }
 }
 
-export { burgerClick, burger };
+burger.addEventListener("click", burgerClick);
--- cards.js
+++ cards.js
@@ -411,5 +411,3 @@ const cards = [
     },
   ],
 ];
-
-export { cards, cardsCategory };
